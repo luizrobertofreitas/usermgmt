bf7765bf34977a03d7f67005c4109128d6934951
Luiz Roberto Freitas <luizrobertofreitas@gmail.com>
2019-10-01 00:11:05 -0300

    Update UserResource.java

--- src/main/java/com/usermgmt/resources/UserResource.java
+++ src/main/java/com/usermgmt/resources/UserResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/v1/users")
+@RequestMapping("/users")
 public class UserResource {
 
   @GetMapping("/ping")
