618dad40d1f8ead42dbbfc23940de171ab5f6abd
Luiz Roberto Freitas <luiz.junior@accurate.com.br>
2019-09-30 23:32:28 -0300

    merge with previous branch

--- src/main/java/com/usermgmt/resources/UserResource.java
+++ src/main/java/com/usermgmt/resources/UserResource.java
@@ -1,18 +1,36 @@
 package com.usermgmt.resources;
 
-import java.time.LocalDateTime;
+import com.usermgmt.model.UserRepository;
+import com.usermgmt.model.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserResource {
 
-  @GetMapping("/ping")
-  public ResponseEntity ping() {
-    return ResponseEntity.ok("Ping at: " + LocalDateTime.now());
+  @Autowired
+  private UserRepository userRepository;
+
+  @PostMapping
+  public ResponseEntity post(@RequestBody final UserEntity user) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity get(@PathVariable("id") final Long id) {
+    return ResponseEntity.ok(userRepository.getOne(id));
+  }
+
+  @GetMapping
+  public ResponseEntity get() {
+    return ResponseEntity.ok(userRepository.findAll());
+  }
 }
