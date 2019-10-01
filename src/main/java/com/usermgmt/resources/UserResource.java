84aba6be2de7c21d6fc55f849d820bad5ad4a4de
Luiz Roberto Freitas <luiz.junior@accurate.com.br>
2019-09-30 23:41:30 -0300

    Full crud support

--- src/main/java/com/usermgmt/resources/UserResource.java
+++ src/main/java/com/usermgmt/resources/UserResource.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/users")
@@ -32,5 +35,16 @@ public class UserResource {
   @GetMapping
   public ResponseEntity get() {
     return ResponseEntity.ok(userRepository.findAll());
+  }
+
+  @PutMapping
+  public ResponseEntity put(@RequestBody final UserEntity user) {
+    return ResponseEntity.ok(userRepository.save(user));
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable("id") final Long id) {
+    userRepository.deleteById(id);
   }
 }
